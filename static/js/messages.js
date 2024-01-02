31d68daad6de07ab7cd2e8d6ed61d8ec09b82c1d
kramislife <egariando619@gmail.com>
2024-01-03 01:32:54 +0800

    modified

--- static/js/messages.js
+++ static/js/messages.js
@@ -10,11 +10,11 @@ progress.classList.add("active");
 
 timer1 = setTimeout(() => {
     toast.classList.remove("active");
-}, 5000);
+}, 3000);
 
 timer2 = setTimeout(() => {
     progress.classList.remove("active");
-}, 5300);
+}, 3300);
 
 closeIcon.addEventListener("click", () => {
     toast.classList.remove("active");
